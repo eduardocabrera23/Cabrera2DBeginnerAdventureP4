52d3be60d82fa33eddcc05a6f733cb0c90f34763
eduardocabrera23 <768550@student.dusd.net>
2025-12-02 11:44:19 -0800

    added script

--- P4CabreraBeginnerAdventure/BeginnerAdventureP4Cabrera/Assets/Scripts/PlayerController.cs
+++ P4CabreraBeginnerAdventure/BeginnerAdventureP4Cabrera/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,16 @@ public class PlayerController : MonoBehaviour
    
     public InputAction MoveAction;
         
-
+Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
         
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        Vector2 move;
     }
 
     // Update is called once per frame
