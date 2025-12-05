d496672a96b25e0c114488b335aee6165a9d7958
eduardocabrera23 <768550@student.dusd.net>
2025-12-04 21:12:56 -0800

    Almost finished the unit of HEALTH
    Left off on the last lesson of the unit on page 3

--- P4CabreraBeginnerAdventure/BeginnerAdventureP4Cabrera/Assets/Scripts/PlayerController.cs
+++ P4CabreraBeginnerAdventure/BeginnerAdventureP4Cabrera/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,12 @@ public class PlayerController : MonoBehaviour
     Vector2 move;
     public float speed = 3.0f;
 
+
     // Variables related to the health system
     public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
     int currentHealth;
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,18 +34,18 @@ public class PlayerController : MonoBehaviour
 
 
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+
     }
     // FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
 
     }
 
 
-    void ChangeHealth (int amount)
+   public void ChangeHealth (int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
